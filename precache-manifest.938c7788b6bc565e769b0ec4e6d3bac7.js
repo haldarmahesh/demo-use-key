4e98b48731f46591eb5a1834dddfbc6e490e4160
Mahesh Haldar <haldar.mahesh@gmail.com>
2018-10-31 09:54:49 +0530

    Updates

---
+++ precache-manifest.938c7788b6bc565e769b0ec4e6d3bac7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8561e37a8d3d79164181",
+    "url": "/demo-use-key/static/js/runtime~main.8561e37a.js"
+  },
+  {
+    "revision": "aae2e1e114f0d69396c4",
+    "url": "/demo-use-key/static/js/main.aae2e1e1.chunk.js"
+  },
+  {
+    "revision": "586914092199b5ea3d8d",
+    "url": "/demo-use-key/static/js/1.58691409.chunk.js"
+  },
+  {
+    "revision": "aae2e1e114f0d69396c4",
+    "url": "/demo-use-key/static/css/main.5a719096.chunk.css"
+  },
+  {
+    "revision": "5da306e2bd0344a4c7f2add13070ed63",
+    "url": "/demo-use-key/index.html"
+  }
+];
